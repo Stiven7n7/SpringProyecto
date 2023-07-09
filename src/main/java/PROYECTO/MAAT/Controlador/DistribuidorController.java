822e4b6f3c2301be8947f2f51c4593a2696b189f
Stiven7n7 <stiven3521@gmail.com>
2023-07-09 14:57:25 -0500

    Implementar funcionalidades de imagen

--- src/main/java/PROYECTO/MAAT/Controlador/DistribuidorController.java
+++ src/main/java/PROYECTO/MAAT/Controlador/DistribuidorController.java
@@ -1,16 +1,28 @@
 package PROYECTO.MAAT.Controlador;
 
 
+import PROYECTO.MAAT.Entidades.Producto;
+import PROYECTO.MAAT.Servicio.SerProducto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/Distribuidor")
 public class DistribuidorController {
 
+    @Autowired
+    private SerProducto serProducto;
+
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+
+        List<Producto> productos = serProducto.findAll();
+        model.addAttribute("productos", productos);
         return "Distribuidor/home";
     }
 
