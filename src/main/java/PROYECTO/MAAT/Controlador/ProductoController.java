f09911087cc0dfe22c4281b15d6d24013c10ba27
Stiven7n7 <stiven3521@gmail.com>
2023-07-09 13:17:16 -0500

    Implementado el guardado de imagenes

--- src/main/java/PROYECTO/MAAT/Controlador/ProductoController.java
+++ src/main/java/PROYECTO/MAAT/Controlador/ProductoController.java
@@ -3,15 +3,15 @@ package PROYECTO.MAAT.Controlador;
 import PROYECTO.MAAT.Entidades.Producto;
 import PROYECTO.MAAT.Entidades.Usuario;
 import PROYECTO.MAAT.Servicio.SerProducto;
+import PROYECTO.MAAT.Servicio.UploadFileService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +22,9 @@ public class ProductoController {
 
     @Autowired
     private SerProducto serProducto;
+
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping("")
     public String show(Model model) {
@@ -35,10 +38,26 @@ public class ProductoController {
     }
 
     @PostMapping("/save")
-    public String save(Producto producto){
+    public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("Este es el objeto producto {}", producto);
         Usuario u = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(u);
+        //imagen
+        if (producto.getId()==null){ //cuando se crea un producto
+            String nombreImagen= upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }
+        else {
+            if(file.isEmpty()){//editamos el producto pero no cambiamos la imagen
+                Producto p = new Producto();
+                p = serProducto.get(producto.getId()).get();
+                producto.setImagen(p.getImagen());
+            }
+            else {
+                String nombreImagen= upload.saveImage(file);
+                producto.setImagen(nombreImagen);
+            }
+        }
         serProducto.save(producto);
         return "redirect:/productos";
     }
