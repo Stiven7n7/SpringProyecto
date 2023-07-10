5149d6959a073574ca84f2836e80a51bc5603065
Stiven7n7 <stiven3521@gmail.com>
2023-07-09 23:01:51 -0500

    Implementando logica del carrito

--- src/main/java/PROYECTO/MAAT/Controlador/HomeController.java
+++ src/main/java/PROYECTO/MAAT/Controlador/HomeController.java
@@ -1,5 +1,7 @@
 package PROYECTO.MAAT.Controlador;
 
+import PROYECTO.MAAT.Entidades.DetalleOrden;
+import PROYECTO.MAAT.Entidades.Orden;
 import PROYECTO.MAAT.Entidades.Producto;
 import PROYECTO.MAAT.Servicio.SerProducto;
 import org.slf4j.Logger;
@@ -7,11 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +23,10 @@ public class HomeController {
 
     @Autowired
     private SerProducto serProducto;
+
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    Orden orden = new Orden();
 
     @GetMapping("")
     public String home(Model model){
@@ -43,7 +48,15 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = serProducto.get(id);
+        log.info("Producto añadido {}", optionalProducto.get());
+        log.info("Cantidad: {}", cantidad);
+
         return "Usuario/carrito.html";
     }
 }
