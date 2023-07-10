ce98b891520d45f2103c8d359735c0093aed7535
Stiven7n7 <stiven3521@gmail.com>
2023-07-09 21:35:02 -0500

    mostrar la vista de producto

--- src/main/java/PROYECTO/MAAT/Controlador/HomeController.java
+++ src/main/java/PROYECTO/MAAT/Controlador/HomeController.java
@@ -1,15 +1,20 @@
 package PROYECTO.MAAT.Controlador;
 
 import PROYECTO.MAAT.Servicio.SerProducto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private SerProducto serProducto;
@@ -19,5 +24,11 @@ public class HomeController {
 
         model.addAttribute("productos", serProducto.findAll());
         return "Usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        log.info("Id enviado como parametro {}", id);
+        return "Usuario/productohome";
     }
 }
